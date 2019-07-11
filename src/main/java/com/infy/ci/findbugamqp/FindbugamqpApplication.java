42e520d6e6d017d36ba30dd3777dd4c5d1bae935
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-11 12:40:47 +0530

    code modify

---
+++ src/main/java/com/infy/ci/findbugamqp/FindbugamqpApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.findbugamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindbugamqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindbugamqpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/ci/findbugamqp/FindbugamqpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infy.ci.findbugamqp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FindbugamqpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
